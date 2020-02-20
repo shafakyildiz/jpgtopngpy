d1bc6b9f8c85e3aac6d74af7c93af328c491c751
Şafak Yıldız <shafakyildiz@gmail.com>
2020-02-21 01:22:52 +0300

    Add files via upload

---
+++ jpg_to_png.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# check is new/ exists if not create it
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# loop through
+# convert images to png
+# save to the new folder
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('all done!')
